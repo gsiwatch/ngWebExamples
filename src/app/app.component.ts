d1fa2f82db518bbcc552c40559c3a46f244198ba
gsiwatch <gaurish.siwatch@gmail.com>
2022-01-01 19:57:46 -0500

    reactive form control example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  title = 'ngWebExample';
+  public firstNameFormControl: FormControl;
+  public lastNameFormControl: FormControl;
+
+  constructor() {
+    this.firstNameFormControl = new FormControl();
+    this.lastNameFormControl = new FormControl();
+  }
 }
