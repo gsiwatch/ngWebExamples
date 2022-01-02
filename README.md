57c9da595d4eb5381a7d3e946a82c0e437cd696b
gsiwatch <gaurish.siwatch@gmail.com>
2022-01-01 21:32:35 -0500

    reactive group example

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # NgWebExample
 We are using angular version 13.1.2 for coding here
+
+# Form Group
+we use form group to group form controls
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  title = 'ngWebExample';
+  myForm: FormGroup = new FormGroup({
+    firstName: new FormControl(),
+    lastName: new FormControl(),
+  });
 }
